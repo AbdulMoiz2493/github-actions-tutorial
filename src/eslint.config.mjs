8f7bbc05d1b6c6f0c24b366d9db216ad730d43bf
AbdulMoiz2493 <abdulmoiz8895@gmail.com>
2025-06-02 19:05:09 +0500

    Updated configurations

--- src/eslint.config.mjs
+++ src/eslint.config.mjs
@@ -4,6 +4,10 @@ import jsonc from "eslint-plugin-jsonc";
 import jest from "eslint-plugin-jest";
 
 export default [
+  // Global ignores to exclude package-lock.json and package.json
+  {
+    ignores: ["package-lock.json", "package.json", "**/package-lock.json", "**/package.json"]
+  },
   {
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { 
