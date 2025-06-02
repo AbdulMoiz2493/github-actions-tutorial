dfa59b6c6b2aba3e53a59671a9363da118515c7b
AbdulMoiz2493 <abdulmoiz8895@gmail.com>
2025-06-02 18:27:36 +0500

    Updated configurations

--- src/eslint.config.mjs
+++ src/eslint.config.mjs
@@ -3,10 +3,24 @@ import globals from "globals";
 import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["plugin:jest/recommended"],
+    languageOptions: {
+      globals: { ...globals.node, ...globals.jest },
+    },
+  },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
 ]);
