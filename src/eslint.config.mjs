baec96da883970b0a7d5cf634a9ab5f7f0958722
AbdulMoiz2493 <abdulmoiz8895@gmail.com>
2025-06-02 18:30:49 +0500

    Updated configurations 2

--- src/eslint.config.mjs
+++ src/eslint.config.mjs
@@ -1,13 +1,19 @@
 import js from "@eslint/js";
 import globals from "globals";
 import json from "@eslint/json";
-import { defineConfig } from "eslint/config";
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export default defineConfig([
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+export default [
+  ...compat.extends("plugin:jest/recommended"),
   {
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
-    extends: ["plugin:jest/recommended"],
     languageOptions: {
       globals: { ...globals.node, ...globals.jest },
     },
@@ -23,4 +29,4 @@ export default defineConfig([
     language: "json/json",
     extends: ["json/recommended"],
   },
-]);
+];
